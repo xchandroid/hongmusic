f712e2b009f5ff5b9b1b7eb3a3bec800f8ef3da2
vaiyee <1208718872@qq.com>
2018-04-21 16:32:21 +0800

    加入收藏歌单功能

---
+++ app/src/main/java/com/vaiyee/hongmusic/Adapter/GedanShouCangAdapter.java
@@ -0,0 +1,10 @@
+package com.vaiyee.hongmusic.Adapter;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Administrator on 2018/4/21.
+ */
+
+public class GedanShouCangAdapter extends BaseAdapter {
+}
---
+++ app/src/main/java/com/vaiyee/hongmusic/Utils/GedanShouCangUtil.java
@@ -0,0 +1,8 @@
+package com.vaiyee.hongmusic.Utils;
+
+/**
+ * Created by Administrator on 2018/4/20.
+ */
+
+public class GedanShouCangUtil {
+}
